a807b2d757bc2bac0c1c326a3e09ea0b80f2462e
RedBirdRacing <96977145+RedBirdRacing@users.noreply.github.com>
2022-02-22 22:54:11 +0800

    Add files via upload

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "wfuai@connect.ust.hk";
+    $email_to = "redbirdracinghkustevrt@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
